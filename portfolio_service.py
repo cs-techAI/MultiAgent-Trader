08100b0c0b382130b614a1165d218107e081c6f0
cs-techAI <cstechaigroup@gmail.com>
2025-06-10 14:19:08 +0530

    Update portfolio_service.py

--- portfolio_service.py
+++ portfolio_service.py
@@ -51,6 +51,7 @@ class PortfolioService:
             response.raise_for_status()
             return response.json()
         
+
         
         except Exception as e:
             return {"error": str(e)}
