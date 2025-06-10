abea39b451c9b0390d40d4ab72e4623c73370429
cs-techAI <cstechaigroup@gmail.com>
2025-06-10 14:28:09 +0530

    Update alpaca_service.py

--- alpaca_service.py
+++ alpaca_service.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class AlpacaService:
+    
     API_KEY = os.getenv("ALPACA_API_KEY")
     SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
     BASE_URL = os.getenv("ALPACA_BASE_URL", "https://paper-api.alpaca.markets")
