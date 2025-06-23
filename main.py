aa33ce301f318c6d419cf7347b5a89002bff9f63
cs-techAI <cstechaigroup@gmail.com>
2025-06-23 19:57:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from agents.router_agent import RouterAgent
 router = RouterAgent()
 print("💬 SmartTrader CLI — type 'exit' to quit.\n")
 
-
-
 while True:
     q = input("You: ")
     if q.lower() == "exit":
